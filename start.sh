9db5839da4ecfae6b5f2508b193401eea4740d75
Chris Elliott <ctelliott@gmail.com>
2015-09-25 01:10:43 -0400

    fixed dir in start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 set -e
 set -u
 
-# migrade db and run rails server on port 5000
-cd /todomvc_on_rails && rake db:migrate && ./bin/rails server -p 5000
+# migrate db and run rails server on port 5000
+# TODO make RAILS_PORT configurable 
+cd /$REPO && rake db:migrate && ./bin/rails server -p 5000
